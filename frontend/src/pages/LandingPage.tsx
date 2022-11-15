7b48be787146cd0c9c4993fe7672c9657c79060a
sagharnoursobhi <saghar.noursobhi@hyperisland.se>
2022-11-15 15:30:25 +0100

    landing page styling

--- frontend/src/pages/LandingPage.tsx
+++ frontend/src/pages/LandingPage.tsx
@@ -5,8 +5,12 @@ export default function LandingPage() {
         <div className="landing-container">
             <div className="heading-container d-flex justify-content-around align-items-center">
                 <div className="content-container">
-                    <h2 className="title">Hi! I am Saghar</h2>
-                    <div className="description">Frontend <span>Developer</span></div>
+                    <div className='introduction'>Welcome to my site</div>
+                    <h2 className="title-first">Hi! I am Saghar</h2>
+                    <div className="title-second">Frontend <span>Developer</span></div>
+                    <div className="description">
+                        I like to craft solid and scalable frontend products with great user experiences.
+                    </div>
                 </div>
                 <div className="image-container">
                     <div className="photo-container"><img src={require('../asset/images/photo.png')} alt="profile" /></div>
